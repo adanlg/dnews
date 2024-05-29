9fe5017a71eb95eaa34392daeef7aab17b1e45c9
adanlg <luisgadan88@gmail.com>
2024-05-29 18:31:06 +0200

    feat: second try

--- src/app/tokenomics/page.tsx
+++ src/app/tokenomics/page.tsx
@@ -21,10 +21,10 @@ const TokenomicsPage = () => {
               <Image src="/tokenomics.png" alt="Tokenomics" layout='fill' objectFit='contain' />
             </div>
             <ul className="list-disc list-inside space-y-2 text-gray-600 text-center">
-              <li><strong>For the writers:</strong> To publish news, you will need to lock a certain amount of the token in the smart contract for the duration of the season, e.g., 1 month. Depending on the news' rating, the writer will either lose the locked amount or earn additional tokens.</li>
+              <li><strong>For the writers:</strong> To publish news, you will need to lock a certain amount of the token in the smart contract for the duration of the season, e.g., 1 month. Depending on the rating of the news, the writer will either lose the locked amount or earn additional tokens.</li>
               <li><strong>For the readers with tokens:</strong> They will earn tokens for contributing to the ranking of the news through thumbs up or down.</li>
               <li><strong>For the readers without tokens:</strong> They can access the news just like reading a regular newspaper, without the need for a wallet or login.</li>
-              <li><strong>For all holders:</strong> The token grants decision-making power within a DAO. Decisions to vote on may include setting the correct locking amount "x", determining the percentage of earnings for readers and writers, deciding on advertising inclusion, and more.</li>
+              <li><strong>For all holders:</strong> The token grants decision-making power within a DAO. Decisions to vote on may include setting the correct locking amount &quot;x&quot;, determining the percentage of earnings for readers and writers, deciding on advertising inclusion, and more.</li>
             </ul>
           </div>
           <div className="max-w-4xl w-full p-6 md:rounded-lg shadow-lg bg-white my-8" style={{ boxShadow: '0 -4px 8px -6px rgba(0,0,0,0.3)' }}>
