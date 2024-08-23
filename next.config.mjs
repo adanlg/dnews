442d71382b1cc82285e87a376e289110541f3b2d
adanlg <luisgadan88@gmail.com>
2024-08-23 03:42:01 +0200

    feat: cloudinary accept http and https

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'img.clerk.com',
-            },
-            {
-              protocol: 'http',
-              hostname: 'res.cloudinary.com',
-            },
-        ],
-    }
+  images:{
+      remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'img.clerk.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+          },
+          {
+            protocol: 'http',
+            hostname: 'res.cloudinary.com',
+          },
+      ],
+  }
 };
 
 export default nextConfig;
